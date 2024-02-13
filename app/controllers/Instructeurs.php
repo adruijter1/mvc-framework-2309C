3e85079ed0237f00404b13733ac5a6b1531a112a
Arjan de Ruijter <rra@mboutrecht.nl>
2024-02-13 09:14:17 +0100

    Laatste commit lesweek 3

--- app/controllers/Instructeurs.php
+++ app/controllers/Instructeurs.php
@@ -3,20 +3,34 @@
 class Instructeurs extends BaseController
 {
 
-
+    /**
+     * Deze index method is er voor de Instructeurs/index view
+     */
     public function index($firstname = NULL, $infix = NULL, $lastname = NULL)
     {
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $data = [
+                'title' => 'Instructeurspagina',
+                'firstname' => $_POST['firstname'],
+                'infix' => $_POST['infix'],
+                'lastname' => $_POST['lastname']
+            ];
+        } else {
+
+        
         /**
          * In het $data-array zet je alle info die de view nodig heeft
          */
         $data = [
             'title' => 'Instructeurspagina',
-            'firstname' => $firstname
+            'firstname' => $firstname,
+            'infix' => $infix,
+            'lastname' => $lastname
         ];
-
+        }
         /**
          * Door het aanroepen van de view-method uit de BaseControllers-class
-         * laat je alle view-gegevens op het scherm 
+         * laad je alle view-gegevens op het scherm 
          */
         $this->view('Instructeurs/index', $data);
     }
--- app/views/Instructeurs/index.php
+++ app/views/Instructeurs/index.php
@@ -1,4 +1,24 @@
+<?php require APPROOT . '/views/includes/header.php'; ?>
+
 <?php
-echo "Hallo " . $data['firstname'];
+echo "Hallo " . $data['firstname'] . " " . $data['infix'] . " " . $data['lastname'];
 
 ?>
+
+<form action="<?= URLROOT . '/Instructeurs/index'; ?>" method="post">
+    <div class="mb-3">
+    <label for="exampleFormControlInput1" class="form-label">Voornaam</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1" name=firstname>
+    </div>
+    <div class="mb-3">
+    <label for="exampleFormControlInput1" class="form-label">Tussenvoegsel</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1" name=infix>
+    </div>
+    <div class="mb-3">
+    <label for="exampleFormControlInput1" class="form-label">Achternaam</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1" name=lastname>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<?php require APPROOT . '/views/includes/footer.php'; ?>
+
